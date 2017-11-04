cf67ef4d40b483843b3514804945a45422462695
Martin-Verweij <32358826+Martin-Verweij@users.noreply.github.com>
2017-11-04 12:04:36 +0100

    class and structs

--- Something new/LIFENEW/Source.cpp
+++ Something new/LIFENEW/Source.cpp
@@ -1,170 +1,84 @@
+#include<iostream>
+#include<fstream>
+#include<string>
 
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <cstdlib>
 
 
 using namespace std;
-//For a space that is 'populated':
-//Each cell with one or no neighbors dies, as if by solitude.
-//Each cell with four or more neighbors dies, as if by overpopulation.
-//Each cell with two or three neighbors survives.
-//For a space that is 'empty' or 'unpopulated'
-//Each cell with three neighbors becomes populated.
-
-//menuvariables
-string menutext = "kies een optie uit: C, R, P, S";
-bool gameover = false;
-bool insubmenu = false;
-
-//fstream
+//ifstream
 ifstream infile;
-bool donereading = false;
-char charin;
-int test;
-bool screenchange = true;
+bool donereading;
+//world
+const int grid = 1000;
+bool world[grid][grid] = { false };
+bool world_[grid][grid] = { false };
 
-//coordinate NOT SURE HOW THIS WORKS 
-struct coordinates {
-	int i; int j;
+//prototypes
+void overwrite_world();
+
+struct position {
+	int i;
+	int j;
 };
-//Define gridSize and screensize
-const int w = 25;
-const int h = 100;
-const int gridSize = 1000;
-
-//Define a cell and world variables
-char cell;
-char alive;
-bool world[gridSize][gridSize] = { false };
-bool worldnew[gridSize][gridSize] = { false };
-////prototypes
-void menu();
-void submenu();
-void draw();
-void fillrandom();
-void matrix();
-int alivecounter(int i, int j);
-
-//matrixnew filler THIS IS SUPPOSED TO CREATE THE NEW MATRIX BASED ON HOW MANY NEIGHBOURS 
-void matrixfiller() {
-	for (int i = 0; i < gridSize + 1; i++) {
-		for (int j = 0; j < gridSize + 1; j++) {
-			bool alive2;
-			if (alivecounter(i, j) == 3) {
-				alive2 = true;
-			}
-			else if ((alivecounter(i, j) == 2) && (world[i][j])) {
-				alive2 = true;
-			}
-			else {
-				alive2 = false;
-			}
-			worldnew[i][j] = alive2;
-		}
-	}
-	matrix();
+struct cell {
+	position pos;
+	bool alive;
+	bool stays_alive;
+	int buren = 0;
+};
+class LIFE {
+	int screen_w = 100;
+	int screen_h = 25;
+	int max_buren = 3;
+	int min_buren = 2;
+public:
+	void cell_initializer(int a, cell &c);
+	void set_position(int a, cell &c);
+	void is_alive(cell &c);
+	void tel_buren(cell &c);
+	void stays_alive(cell &c);
+};
+void LIFE::set_position(int a, cell &c) {
+	c.pos.i = a / grid;
+	c.pos.j = a % grid;
 }
-//replace frames
-void matrix() {
-	for (int i = 0; i < gridSize + 1; i++) {
-		for (int j = 0; j < gridSize + 1; j++) {
-			world[i][j] = worldnew[i][j];
-		}
-	}
-	worldnew[gridSize][gridSize] = { false };
+void LIFE::is_alive(cell &c) {
+	c.alive = world[c.pos.i][c.pos.j];
 }
-
-////current menu////
-void currentmenu() {
-	draw();
-	if (!insubmenu) {
-		menu();
+void LIFE::cell_initializer(int a, cell &c) {
+	set_position(a, c); is_alive(c); tel_buren(c); stays_alive(c);
+}
+void LIFE::tel_buren(cell &c) {
+	for (int i = -1; i < 2; i++) {
+		for (int j = -1; j < 2; j++) {
+			cell buur;
+			buur.pos = { c.pos.i+i, c.pos.j+j }; is_alive(buur);
+			if (buur.pos.i < 0) { i++; }
+			else if (buur.pos.j < 0) { j++; }
+			else if (buur.alive) {
+				c.buren++;
+			}
+		}
+	} if (c.alive) { c.buren--; }
+}
+void LIFE::stays_alive(cell &c) {
+	if (c.buren == max_buren) {
+		c.stays_alive = true;
+	}
+	else if (c.buren == min_buren && c.alive) {
+		c.stays_alive = true;
 	}
 	else {
-		submenu();
+		c.stays_alive = false;
 	}
-}
-////submenu-function////
-void submenu() {
-	insubmenu = true;
-	char input = cin.get();
-	do {
-		switch (input) {
-		case 'b':
-			fillrandom();
-		case 't':
-		case 'T':
-			insubmenu = false;
-			return;
-		}
-		input = cin.get();
-	} while (input != 't' || input != 'T');
-}
-////parameters function////
-void parameters() {
-	menutext = "dit is een submenu optie 't' om terug te gaan";
-	draw();
-	submenu();
-}
-////clear function////
-void clear() {
-	cout << 'text' << endl;
-}
-////rename function////
-void rename() {
-	cout << "text" << endl;
-}
-
-////main menu////
-void menu() {
-	menutext = "kies een optie uit: C, R, P, S";
-	draw();
-	char input = cin.get();
-	do {
-		switch (input) {
-		case 'c':
-		case 'C':
-			clear();
-			return;
-		case 'p':
-		case 'P':
-			parameters();
-			return;
-		case 'r':
-		case 'R':
-			rename();
-			return;
-		case 's':
-		case 'S':
-			gameover = true;
-			return;
-
-		}
-		input = cin.get();
-	} while ((input != 's') || (input != 'S'));
-}
-////number parser////
-int filternum() {
-	char input = cin.get();
-	int num = 0;
-	int numbercounter = 0;
-	while (numbercounter < 3) {
-		if (isdigit(input)) {
-			num << input;
-			numbercounter++;
-		}
-	}
-	return num;
 }
 //file reader
 char readfile() {
-	charin = infile.get();
+	char charin = infile.get();
 	if (!donereading) {
 		if (infile.eof()) {
-			infile.close();
 			donereading = true;
+			infile.close();
 			return 'e';
 		}
 		else {
@@ -173,86 +87,76 @@ char readfile() {
 	}
 	return 'e';
 }
-
-//fill random
-void fillrandom() {
-	for (int c = 0; c < (gridSize * gridSize) + 1; c++) {
-		for (int i = 0; i < gridSize; i++) {
-			for (int j = 0; j < gridSize; j++) {
+char displaychar(bool b) {
+	char c;
+	if (b) {
+		c = '@';
+	}
+	else {
+		c = '_';
+	}
+	return c;
+}
+void fill_world() {
+	for (int c = 0; c < (grid * grid) + 1; c++) {
+		for (int i = 0; i < grid; i++) {
+			for (int j = 0; j < grid; j++) {
 				char infilechar = readfile();
 				if (infilechar != 'e') {
 					if (infilechar == ' ') {
-						alive = false;
+						world[i][j] = false;
 					}
 					else if (infilechar == '\n') {
+						world[i][j] = false;
 						i++; j = -1;
-						alive = false;
 					}
 					else {
-						alive = true;
+						world[i][j] = true;
 					}
-					world[i][j] = alive;
 				}
-				else {
-					return;
-				}
+				else return;
 			}
 		}
 	}
 }
-
-char displaychar(bool alive) {
-	if (alive) {
-		cell = 'X';
-	}
-	else {
-		cell = ' ';
-	}
-	return cell;
-}
-
-//Create the display in console 
 void draw() {
-	for (int x = 0; x < w; x++) {
-		for (int y = 0; y < h; y++) {
-			cout << displaychar(world[x][y]); //IF YOU WANT TO SEE THE AMOUNT OF NEIGHBOURS CHANGE THE COUT TO alivecounter(x,y)
-		} cout << endl;
-	}cout << menutext << endl;
-	screenchange = false;
+	cout << string(100, '\n');
+	static char buffer[25 * 100];
+	char * p_next_write = &buffer[0];
+	int c = 0;
+	for (int x = 0; x < 25; x++) {
+		for (int y = 0; y < 100; y++) {
+			*p_next_write++ = displaychar(world[x][y]);
+		} 
+		*p_next_write++ = '\n';
+	}cout.write(&buffer[0], 25*100);
 }
-//counts live neighbours
-int alivecounter(int i, int j) {
-	int neighbour_counter = 0;
-	bool neighbour_state;
-	for (int x = -1; x < 2; x++) {
-		for (int y = -1; y < 2; y++) {
-			if (i + x < 0 || j + y < 0 || i + x > gridSize || j + y > gridSize) {
-				neighbour_state = false;
-			}
-			else {
-				neighbour_state = world[i + x][j + y];
-			}
-			if (neighbour_state) {
-				neighbour_counter++;
-			}
-		}
+void single_gen() {
+	LIFE G;
+	for (int g = 0; g < grid * grid; g++) {
+		cell cell;
+		G.cell_initializer(g, cell);
+		world[cell.pos.i][cell.pos.j] = cell.alive;
+		world_[cell.pos.i][cell.pos.j] = cell.stays_alive;
 	}
-	if (world[i][j]) {
-		neighbour_counter--;
-	}
-	return neighbour_counter;
+	overwrite_world();
 }
-//main
+void overwrite_world() {
+	for (int c = 0; c < grid*grid; c++) {
+		position p = { c / grid,c % grid };
+		world[p.i][p.j] = world_[p.i][p.j];
+	}
+}
 int main() {
-	infile.open("infile.txt");
 	int test;
-	fillrandom();
-	draw();
-	for (int a = 0; a < 100; a++) {
-		matrixfiller();
-		draw();
+	LIFE M;
+	infile.open("infile.txt");
+	fill_world();
+	cin >> test;
+	for (int i = 0; i < test; i++) {
+		
+		single_gen();draw();
 	}
 	cin >> test;
-
 	return 0;
 }
